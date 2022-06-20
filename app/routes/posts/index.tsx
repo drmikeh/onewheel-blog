69378f2b85b329c874b79758ced323186d94c43f
Dr. Mike Hopper <drmikeh@gmail.com>
2022-06-20 16:38:27 -0400

    Completes through video lesson 7

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,30 @@
+import { Link, useLoaderData } from '@remix-run/react'
+import type { LoaderFunction } from '@remix-run/server-runtime'
+import { json } from '@remix-run/server-runtime'
+import { getPosts } from '~/models/post.server'
+
+type LoaderData = {
+    posts: Awaited<ReturnType<typeof getPosts>>
+}
+
+export const loader: LoaderFunction = async () => {
+    const posts = await getPosts()
+    return json<LoaderData>({ posts })
+}
+
+export default function PostsRoute() {
+    const { posts } = useLoaderData() as LoaderData
+
+    return (
+        <main className="m-8">
+            <h1 className="mb-4 text-4xl text-blue-800">Posts</h1>
+            {posts.map(post => (
+                <li key={post.slug}>
+                    <Link to={post.slug} className="text-blue-600 underline">
+                        {post.title}
+                    </Link>
+                </li>
+            ))}
+        </main>
+    )
+}
