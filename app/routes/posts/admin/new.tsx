e7b9635a747254405123805f2b5c02df7c24a5ec
Dr. Mike Hopper <drmikeh@gmail.com>
2022-06-21 09:55:14 -0400

    Completes video lesson 15

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -2,7 +2,16 @@ import { Form, useActionData } from '@remix-run/react'
 import type { ActionFunction } from '@remix-run/server-runtime'
 import { json } from '@remix-run/server-runtime'
 import { redirect } from '@remix-run/server-runtime'
+import invariant from 'tiny-invariant'
 import { createPost } from '~/models/post.server'
+
+type ActionData =
+    | {
+          title: null | string
+          slug: null | string
+          markdown: null | string
+      }
+    | undefined
 
 export const action: ActionFunction = async ({ request }) => {
     const formData = await request.formData()
@@ -11,7 +20,7 @@ export const action: ActionFunction = async ({ request }) => {
     const slug = formData.get('slug')
     const markdown = formData.get('markdown')
 
-    const errors = {
+    const errors: ActionData = {
         title: title ? null : 'Title is required',
         slug: slug ? null : 'Slug is required',
         markdown: markdown ? null : 'Markdown is required',
@@ -19,8 +28,12 @@ export const action: ActionFunction = async ({ request }) => {
 
     const hasErrors = Object.values(errors).some(errorMessage => errorMessage)
     if (hasErrors) {
-        return json(errors)
+        return json<ActionData>(errors)
     }
+
+    invariant(typeof title === 'string', 'title must be a string')
+    invariant(typeof slug === 'string', 'slug must be a string')
+    invariant(typeof markdown === 'string', 'markdown must be a string')
 
     await createPost({ title, slug, markdown })
     return redirect('/posts/admin')
@@ -29,7 +42,7 @@ export const action: ActionFunction = async ({ request }) => {
 const inputClassName = 'w-full rounded border border-gray-500 px-2 py-4'
 
 export default function NewPostRoute() {
-    const errors = useActionData()
+    const errors = useActionData() as ActionData
     return (
         <Form method="post">
             <p>
