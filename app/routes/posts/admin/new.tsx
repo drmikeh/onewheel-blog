1b24ab6db8f684bacc3e8675a232254892c70794
Dr. Mike Hopper <drmikeh@gmail.com>
2022-06-21 10:07:04 -0400

    Completes video lesson 16

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,4 +1,4 @@
-import { Form, useActionData } from '@remix-run/react'
+import { Form, useActionData, useTransition } from '@remix-run/react'
 import type { ActionFunction } from '@remix-run/server-runtime'
 import { json } from '@remix-run/server-runtime'
 import { redirect } from '@remix-run/server-runtime'
@@ -43,6 +43,10 @@ const inputClassName = 'w-full rounded border border-gray-500 px-2 py-4'
 
 export default function NewPostRoute() {
     const errors = useActionData() as ActionData
+
+    const transition = useTransition()
+    const isCreating = Boolean(transition.submission)
+
     return (
         <Form method="post">
             <p>
@@ -84,8 +88,9 @@ export default function NewPostRoute() {
             <p className="text-right">
                 <button
                     type="submit"
-                    className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-300">
-                    Create Post
+                    className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+                    disabled={isCreating}>
+                    {isCreating ? 'Creating...' : 'Create Post'}
                 </button>
             </p>
         </Form>
