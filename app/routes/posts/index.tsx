a2c787969d4ce926d490d31421d7ff2039f3475d
Dr. Mike Hopper <drmikeh@gmail.com>
2022-06-21 20:23:27 -0400

    Completes video lesson 19

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -2,6 +2,7 @@ import { Link, useLoaderData } from '@remix-run/react'
 import type { LoaderFunction } from '@remix-run/server-runtime'
 import { json } from '@remix-run/server-runtime'
 import { getPostListings } from '~/models/post.server'
+import { useOptionalAdminUser } from '~/utils'
 
 type LoaderData = {
     posts: Awaited<ReturnType<typeof getPostListings>>
@@ -14,13 +15,16 @@ export const loader: LoaderFunction = async () => {
 
 export default function PostsRoute() {
     const { posts } = useLoaderData() as LoaderData
+    const adminUser = useOptionalAdminUser()
 
     return (
         <main>
             <h1 className="mb-4 text-4xl text-blue-800">Posts</h1>
-            <Link to="admin" className="text-red-600 underline">
-                Admin
-            </Link>
+            {adminUser ? (
+                <Link to="admin" className="text-red-600 underline">
+                    Admin
+                </Link>
+            ) : null}
             {posts.map(post => (
                 <li key={post.slug}>
                     <Link
