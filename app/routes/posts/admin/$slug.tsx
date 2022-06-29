6063e53692fa7f55ef397f1c1b4a317f36f93648
Dr. Mike Hopper <drmikeh@gmail.com>
2022-06-29 09:45:51 -0400

    Completes video lesson 26

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -1,7 +1,9 @@
 import {
     Form,
     useActionData,
+    useCatch,
     useLoaderData,
+    useParams,
     useTransition,
 } from '@remix-run/react'
 import type { ActionFunction, LoaderFunction } from '@remix-run/server-runtime'
@@ -26,6 +28,9 @@ export const loader: LoaderFunction = async ({ request, params }) => {
         return json<LoaderData>({})
     }
     const post = await getPost(params.slug)
+    if (!post) {
+        throw new Response('Not Found', { status: 404 })
+    }
     return json<LoaderData>({ post })
 }
 
@@ -163,4 +168,17 @@ export default function NewPostRoute() {
             </div>
         </Form>
     )
+}
+
+export function CatchBoundary() {
+    const caught = useCatch()
+    const params = useParams()
+    if (caught.status === 404) {
+        return (
+            <div className="center text-xl text-red-600">
+                Uh oh! Th post with the slug "{params.slug}" does not exist!
+            </div>
+        )
+    }
+    throw new Error(`Unsupported thrown response status code: ${caught.status}`)
 }
