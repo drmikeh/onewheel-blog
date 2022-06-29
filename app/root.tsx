a16f89b43a0c2bd0afc522e9852c6f14cc2b501b
Dr. Mike Hopper <drmikeh@gmail.com>
2022-06-29 11:56:54 -0400

    Changes title for HTML page.

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
     charset: 'utf-8',
-    title: 'Remix Notes',
+    title: 'Remix Blog',
     viewport: 'width=device-width,initial-scale=1',
 })
 
