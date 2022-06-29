7ef1272822c6ed045b76c9ae3bb552ef11073856
Dr. Mike Hopper <drmikeh@gmail.com>
2022-06-29 10:48:55 -0400

    Completes video lesson 27

--- app/routes/posts/admin.tsx
+++ app/routes/posts/admin.tsx
@@ -42,3 +42,15 @@ export default function AdminRoute() {
         </div>
     )
 }
+
+export function ErrorBoundary({ error }: { error: unknown }) {
+    if (error instanceof Error) {
+        return (
+            <div className="text-red-500">
+                Oh no, something went wrong!
+                <pre>{error.message}</pre>
+            </div>
+        )
+    }
+    return <div className="text-red-500">Oh no, something went wrong!</div>
+}
